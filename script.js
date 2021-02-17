46192f0bb789cb22b251166c853dc267bd8f816c
Fabio Amaral <fabioamaral.93jr@gmail.com>
2021-02-17 15:58:51 -0300

    adding functions: addingTask, selectingTask and completingTask

---
+++ script.js
@@ -0,0 +1,43 @@
+const btnAdd = document.querySelector('#criar-tarefa');
+const btnDeleteAll = document.querySelector('#apaga-tudo');
+const inputTask = document.querySelector('#texto-tarefa');
+const orderedList = document.querySelector('#lista-tarefas');
+
+//Função para adicionar tarefas à lista
+function addingTask() {
+    if (inputTask.value === '') {
+        alert('Campo vazio');
+    }
+    else {
+        var task = document.createElement('li');
+        task.onclick=selectingTask;
+        task.ondblclick=completingTask;
+        task.innerHTML = inputTask.value;        
+        orderedList.appendChild(task);
+        inputTask.value = "";
+    }
+}
+
+btnAdd.onclick = addingTask;
+
+//Função para alterar a cor de fundo de uma tarefa que foi selecionada pelo usuário
+function selectingTask (event) {
+        const liTarget = event.target;
+        const checkSelected = document.querySelector('.selected');
+        if (checkSelected) {
+            checkSelected.classList.remove('selected');
+        }
+
+        liTarget.classList.add('selected');
+    }
+
+    //Função para tachar itens concluídos
+    function completingTask(event){
+        const liTarget = event.target;
+        if (liTarget.classList.contains('completed')){
+            liTarget.classList.remove('completed');
+        }
+        else{
+            liTarget.classList.add('completed')
+        }        
+    }
